9f11e44f2badd81bbdca7617d885dd1d5f2fbcd3
jahidshawon19 <jahidshawon1730@gmail.com>
2022-06-02 18:32:08 +0600

    issue

--- Stackblitz_ERP/settings.py
+++ Stackblitz_ERP/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$i^x7&qc8d3_*#d_zh_u-3r)81)9-=_&77+yuu%y7@347^s(%='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['pappu-vhai.herokuapp.com','127.0.0.1:8000']
+ALLOWED_HOSTS = ['pappu-vhai.herokuapp.com','127.0.0.1']
 
 
 # Application definition
